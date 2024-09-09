ed96ee516857bacf71fe07ddc90b41b3c027e6bf
katesamuel <csamuel.bi@gmail.com>
2024-09-09 02:20:00 +0100

    home page - big screens

---
+++ src/assets/content/home.ts
@@ -0,0 +1,6 @@
+export const HomeContent = {
+  welcome: 'Hello and welcome to my webpage!',
+  shortSummary: 'I am a choral and orchestral conductor, musician and educator based in Coimbatore, South India. Checkout the ensembles that I am associated with and their music production in the following pages.',
+  invite: 'Please feel free to contact me if you would love to collaborate for productions, concerts, or for guidance to pursue music or simply have any questions about what I do.',
+  thanks: 'Thank you for visiting.'
+};
--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,9 +1,23 @@
+import HeadShot from "../assets/images/faith.png";
+import "./home.css";
+import { HomeContent } from "../assets/content/home";
+
 const Home = () => {
-    return (
-        <div>
-            Home page works!!
-        </div>
-    )
+  return (
+    <div className="home-container">
+      <div className="welcome-note">
+        <p>{HomeContent.welcome}</p>
+        <p>{HomeContent.shortSummary}</p>
+        <p>{HomeContent.invite}</p>
+        <p>{HomeContent.thanks}</p>
+        <br/>
+        <p className="sign">Faith Ragland</p>
+      </div>
+      <div className="headshot-container">
+        <img src={HeadShot} alt="Head Shot" className="headshot-img"></img>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
